1ed9e54f0805900720d1fc35f709ad27238df75b
Spencer Johnson <spencerjames.johnson@students.salle.url.edu>
2023-10-23 18:51:02 +0200

    changing game name to game number

--- BattleArena/src/Views/ArenaManage.vue
+++ BattleArena/src/Views/ArenaManage.vue
@@ -63,7 +63,7 @@
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
         <tr>
           <th scope="col" class="px-6 py-3">
-            Game Number
+            Game ID
           </th>
           <th scope="col" class="px-6 py-3">
             Matrix Size
