e654022e3320cee15bc31532a1dda3e35df93297
Spencer Johnson <spencerjames.johnson@students.salle.url.edu>
2023-10-30 11:08:39 +0100

    playing around with the addition of backgrounds to some screens

--- BattleArena/src/Views/ArenaManage.vue
+++ BattleArena/src/Views/ArenaManage.vue
@@ -14,15 +14,16 @@ const navigateTo = (page) => {
 </script>
 
 <template>
-  <div class="flex-row mx-auto text-gray-600">
+  <body>
+  <div class="flex-col">
+  <div class="flex-row mx-auto justify-around items-center text-gray-600">
       <div class="flex justify-around items-center w-full pb-4 bg-white dark:bg-gray-900">
-        <label for="table-search" class="sr-only">Search</label>
-          <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
             <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
             </svg>
-          </div>
-          <input type="text" id="table-search" class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for Arena">
+
+        <label for="table-search" class="sr-only">Search</label>
+        <input type="text" id="table-search" class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for Arena">
 
 
         <div>
@@ -63,10 +64,19 @@ const navigateTo = (page) => {
         subtitle: 'Here is the list of the Arena\'s and their contents.', 
         columns: ['Game ID', 'Matrix Size', 'Date Created', 'Number of Players', 'Status'] }"
     />
-
-    
+`</div>
+  </body>
 </template>
 
 <style scoped>
+
+body {
+  background-image: url("Photos/dojo.jpg");
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  width: 100vw;
+  height: 100vh;
+}
 
 </style>
--- BattleArena/src/Views/GameArena.vue
+++ BattleArena/src/Views/GameArena.vue
@@ -1,35 +1,61 @@
 <script setup>
 
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const navigateTo = (page) => {
+  router.push(`/${page}`);
+};
+
 </script>
+
+
+
 
 <template>
 
-  <!-- laying out page horizontally-->
-  <div class="flex flex-row items-center space-x-5 h-full pb-4 bg-white dark:bg-gray-900">
-    <img src="/Photos/arena_template.png" class="h-75 mr-3" alt="new arena" />
-    <!-- laying out page vertically on right portion-->
-    <div class="flex flex-col items-center space-y-7 h-full pb-4 bg-white dark:bg-gray-900">
-      <!-- player 1 layout-->
-      <div class="flex flex-col items-center space-y-2 h-full pb-4 bg-white dark:bg-gray-900">
-        <img src="/Photos/rick.png" class="h-20 mr-3" alt="new arena" />
+  <body>
+  <div class="flex flex-col items-center space-x-5 h-full pb-4 bg-white dark:bg-gray-900">
+    <!-- laying out page horizontally-->
+    <div class="flex flex-row items-center space-x-5 h-full pb-4 bg-white dark:bg-gray-900">
+      <img src="/Photos/arena_template.png" class="h-75 mr-3" alt="new arena" />
+      <!-- laying out page vertically on right portion-->
+      <div class="flex flex-col items-center space-y-7 h-full pb-4 bg-white dark:bg-gray-900">
+        <!-- player 1 layout-->
+        <div class="flex flex-col items-center space-y-2 h-full pb-4 bg-white dark:bg-gray-900">
+          <img src="/Photos/rick.png" class="h-20 mr-3" alt="new arena" />
 
-        <!--<p class="text-gray-500 dark:text-gray-400">Player 1</p>
-        leaving above in case its not a chat box and its a button to the profile -->
+          <!--<p class="text-gray-500 dark:text-gray-400">Player 1</p>
+          leaving above in case its not a chat box and its a button to the profile -->
 
-        <label for="player_one_chat" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Player 1</label>
-        <input type="text" id="player_one_chat" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="chat" required>
-      </div>
-      <!-- player 2 layout-->
-      <div class="flex flex-col items-center space-y-2 h-full pb-4 bg-white dark:bg-gray-900">
-        <img src="/Photos/kitty.jpeg" class="h-20 mr-3" alt="new arena" />
-        <label for="player_two_chat" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Player 2</label>
-        <input type="text" id="player_two_chat" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="chat" required>
+          <label for="player_one_chat" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Player 1</label>
+          <input type="text" id="player_one_chat" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="chat" required>
+        </div>
+        <!-- player 2 layout-->
+        <div class="flex flex-col items-center space-y-2 h-full pb-4 bg-white dark:bg-gray-900">
+          <img src="/Photos/kitty.jpeg" class="h-20 mr-3" alt="new arena" />
+          <label for="player_two_chat" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Player 2</label>
+          <input type="text" id="player_two_chat" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="chat" required>
+        </div>
       </div>
     </div>
-
+    <button class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800" @click="navigateTo('GameArena')">End Game</button>
   </div>
+
+
+  </body>
 </template>
 
 <style scoped>
+
+body {
+  background-image: url("Photos/arena_background.jpg");
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  width: 100vw;
+  height: 100vh;
+}
 
 </style>
