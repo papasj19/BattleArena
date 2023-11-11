482b1063ffaf788673d0a3f6d58085c6df7f46d7
Yera66 <yerassyl.omirserik@students.salle.url.edu>
2023-11-11 21:18:22 +0100

    responsive LogSign and Profile

--- BattleArena/src/Views/LogSign.vue
+++ BattleArena/src/Views/LogSign.vue
@@ -1,101 +1,109 @@
 <template>
   <main class="flex justify-center items-center flex-col sm:flex-row h-screen" style="background-image: url('src/assets/photos/cozy-room.png'); background-size: cover;">
-    <div class="p-4">
-      <button @click="showSignupModal" style="width:auto;">Sign Up</button>
 
-      <!-- Sign Up -->
+    <!-- Sign Up Section -->
+    <div class="p-4 w-full sm:w-1/2"> 
       <div v-if="isSignupModalOpen" class="modal">
-        <span @click="closeModal('signup')" class="close" title="Close Modal">&times;</span>
         <form class="modal-content" action="/action_page.php">
           <div class="container">
             <section class="bg-transparent dark:bg-transparent"> 
               <div class="flex flex-col items-center justify-center px-6 py-4 mx-auto md:h-screen lg:py-0"> 
-      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                  Sign up
-              </h1>
-              <form class="space-y-4 md:space-y-6" action="#">
-                  <div>
-                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
-                  </div>
-                  <div>
-                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
-                  </div>
-                  <div>
-                      <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                      <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
-                  </div>
-                  <div class="flex items-start">
-                      <div class="flex items-center h-5">
-                        <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">
+                <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                  <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <!-- Sign Up Title -->
+                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                      Sign up
+                    </h1>
+                    <!-- Sign Up Form -->
+                    <form class="space-y-4 md:space-y-6" action="#">
+                      <div>
+                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                        <!-- Email Input -->
+                        <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
                       </div>
-                      <div class="ml-3 text-sm">
-                        <label for="terms" class="font-light text-gray-500 dark:text-gray-300">I accept the <a class="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
+                      <!-- Password Input -->
+                      <div>
+                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                        <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
                       </div>
+                      <!-- Confirm Password Input -->
+                      <div>
+                        <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
+                        <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                      </div>
+                      <div class="flex items-start">
+                        <!-- Terms and Conditions Checkbox -->
+                        <div class="flex items-center h-5">
+                          <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">
+                        </div>
+                        <div class="ml-3 text-sm">
+                          <!-- Terms and Conditions Label -->
+                          <label for="terms" class="font-light text-gray-500 dark:text-gray-300">I accept the <a class="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
+                        </div>
+                      </div>
+                      <!-- Sign Up Button -->
+                      <button type="submit" class="w-full text-white bg-gray-500 hover:bg-gray-600 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-500 dark:hover:bg-gray-600 dark:focus:ring-gray-800">
+                        Sign up
+                      </button>                  
+                    </form>
                   </div>
-                  <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign up</button>
-                  
-              </form>
-          </div>
-      </div>
-  </div>
-</section>
-            <div class="clearfix">
-              <button type="button" @click="closeModal('signup')" class="cancelbtn">Cancel</button>
-            </div>
+                </div>
+              </div>
+            </section>
           </div>
         </form>
       </div>
     </div>
 
-    <div class="p-4">
-      <button @click="showLoginModal" style="width:auto;">Log In</button>
-
-      <!-- Log In -->
+    <!-- Log In Section -->
+    <div class="p-4 w-full sm:w-1/2"> 
       <div v-if="isLoginModalOpen" class="modal">
-        <span @click="closeModal('login')" class="close" title="Close Modal">&times;</span>
         <form class="modal-content" action="/action_page.php">
           <div class="container">
             <section class="bg-transparent dark:bg-transparent"> 
               <div class="flex flex-col items-center justify-center px-6 py-4 mx-auto md:h-screen lg:py-0">      
-      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                  Log in to your account
-              </h1>
-              <form class="space-y-4 md:space-y-6" action="#">
-                  <div>
-                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
-                  </div>
-                  <div>
-                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
-                  </div>
-                  <div class="flex items-center justify-between">
-                      <div class="flex items-start">
+                <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                  <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <!-- Log In Title -->
+                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                      Log in 
+                    </h1>
+                    <!-- Log In Form -->
+                    <form class="space-y-4 md:space-y-6" action="#">
+                      <!-- Email Input -->
+                      <div>
+                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                        <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
+                      </div>
+                      <!-- Password Input -->
+                      <div>
+                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                        <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                      </div>
+                      <!-- Remember Me Checkbox and Forgot Password Link -->
+                      <div class="flex items-center justify-between">
+                        <div class="flex items-start">
+                          <!-- Remember Me Checkbox -->
                           <div class="flex items-center h-5">
                             <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">
                           </div>
                           <div class="ml-3 text-sm">
+                            <!-- Remember Me Label -->
                             <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
                           </div>
+                        </div>
+                        <!-- Forgot Password Link -->
+                        <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                       </div>
-                      <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                      <!-- Log In Button -->
+                      <button type="submit" class="w-full text-white bg-gray-500 hover:bg-gray-600 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-500 dark:hover:bg-gray-600 dark:focus:ring-gray-800">
+                        Log in
+                      </button>
+                    </form>
                   </div>
-                  <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Log in</button>
-
-              </form>
-          </div>
-      </div>
-  </div>
-</section>
-            <div class="clearfix">
-              <button type="button" @click="closeModal('login')" class="cancelbtn">Cancel</button>
-            </div>
+                </div>
+              </div>
+            </section>
           </div>
         </form>
       </div>
@@ -107,26 +115,9 @@
 export default {
   data() {
     return {
-      isSignupModalOpen: false,
-      isLoginModalOpen: false,
+      isSignupModalOpen: true,
+      isLoginModalOpen: true,
     };
-  },
-  methods: {
-    showSignupModal() {
-      this.isSignupModalOpen = true;
-    },
-    showLoginModal() {
-      this.isLoginModalOpen = true;
-    },
-    closeModal(type) {
-      if (type === 'signup') {
-        this.isSignupModalOpen = false;
-      } else if (type === 'login') {
-        this.isLoginModalOpen = false;
-      }
-    },
   },
 };
 </script>
-
-
--- BattleArena/src/Views/Profile.vue
+++ BattleArena/src/Views/Profile.vue
@@ -1,126 +1,112 @@
 <template>
+  <main class="flex justify-center items-center flex-col sm:flex-row h-screen" style="background-image: url('src/assets/photos/tatami-room.jpg'); background-size: cover;">
 
-  <main class="flex justify-center items-center flex-col sm:flex-row h-screen" 
-  style="background-image: url('src/assets/photos/tatami-room.jpg'); background-size: cover;"
-  >
-      <div class="p-4">
-        <div class="flex items-center space-x-4 bg-white p-2 rounded-lg">
-          <img class="w-20 h-20 rounded" src="src/assets/photos/SJ_cat.jpeg" alt="Large avatar">
-          <div class="text-left font-medium text-black">
+    <!-- Profile Information -->
+    <div class="p-4">
+      <div class="flex items-center space-x-4 bg-white p-2 rounded-lg">
+        <img class="w-20 h-20 rounded" src="src/assets/photos/SJ_cat.jpeg" alt="Large avatar">
+        <div class="text-left font-medium text-black">
           <div>Garfield (Name)</div>
           <p class="text-white-500 light:text-white-400">Based cat. Basic INFO</p>
         </div>
       </div>
 
-  
-        <div class="p-4 text-left">
-          <div class="profile screen">
-            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-              <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                  <tr>
-                    <th scope="col" class="px-6 py-3">
-                      Equipped Attacks
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                      <span class="sr-only">Actions</span>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr v-for="(attack, index) in attacks" :key="index" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                    <td scope="row" class="px-6 py-4">
-                      <input v-model="attack.name" class="w-full border border-gray-300 p-1">
-                    </td>
-                    <td class="px-6 py-4 text-right">
-                      <button @click="editAttack(index)" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</button>
-                      <button @click="deleteAttack(index)" class="font-medium text-red-600 dark:text-red-500 hover:underline">Delete</button>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            <div class="text-center mt-4">
-              <button @click="addNewAttack" class="bg-green-500 text-white font-medium px-3 py-1 rounded hover:bg-green-600">Add Attack</button>
-            </div>
-          </div>
-        </div>
-      </div>
-  
-      <div class="p-4">
+      <!-- Equipped Attacks Table -->
+      <div class="p-4 text-left">
         <div class="profile screen">
           <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
             <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
               <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
-                  <th scope="col" class="px-6 py-3">
-                    Backpacked Attacks
-                  </th>
-                  <th scope="col" class="px-6 py-3">
-                    <span class="sr-only">Edit</span>
-                  </th>
+                  <th scope="col" class="px-6 py-3">Equipped Attacks</th>
+                  <th scope="col" class="px-6 py-3"><span class="sr-only">Actions</span></th>
                 </tr>
               </thead>
               <tbody>
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                  <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Light Attack
-                  </th>
-                  <td class="px-6 py-4 text-right">
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                <tr v-for="(attack, index) in attacks" :key="index" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                  <td scope="row" class="px-6 py-4">
+                    <input v-model="attack.name" class="w-full border border-gray-300 p-1">
                   </td>
-                </tr>
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                  <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Heavy Attack
-                  </th>
                   <td class="px-6 py-4 text-right">
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                  </td>
-                </tr>
-                <tr class="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover-bg-gray-600">
-                  <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Special Attack
-                  </th>
-                  <td class="px-6 py-4 text-right">
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                    <button @click="editAttack(index)" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</button>
+                    <button @click="deleteAttack(index)" class="font-medium text-red-600 dark:text-red-500 hover:underline">Delete</button>
                   </td>
                 </tr>
               </tbody>
             </table>
           </div>
           <div class="text-center mt-4">
-            <button @click="deleteAccount" class="bg-red-500 text-white font-medium px-3 py-1 rounded hover:bg-red-600">Delete Account</button>
+            <button @click="addNewAttack" class="bg-green-500 text-white font-medium px-3 py-1 rounded hover:bg-green-600">Add Attack</button>
           </div>
         </div>
       </div>
-    </main>
-  </template>
-  <script>
-  
-  export default {
-    data() {
-      return {
-        attacks: [
-          { name: 'Light Attack' },
-          { name: 'Heavy Attack' },
-          { name: 'Special Attack' },
-        ],
-      };
-      },
-      methods: {
-        editAttack(index) {
-          // Handle editing logic, e.g., send data to a server or store changes
-        },
-        deleteAttack(index) {
-          this.attacks.splice(index, 1);
-        },
-        addNewAttack() {
-          this.attacks.push({ name: '' });
-        },
-        deleteAccount() {
-          // Handle account deletion logic here
-        },
-      },
+    </div>
+
+    <!-- Backpacked Attacks Table -->
+    <div class="p-4">
+      <div class="profile screen">
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" class="px-6 py-3">Backpacked Attacks</th>
+                <th scope="col" class="px-6 py-3"><span class="sr-only">Edit</span></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">Light Attack</th>
+                <td class="px-6 py-4 text-right">
+                  <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+              </tr>
+              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">Heavy Attack</th>
+                <td class="px-6 py-4 text-right">
+                  <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+              </tr>
+              <tr class="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover-bg-gray-600">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">Special Attack</th>
+                <td class="px-6 py-4 text-right">
+                  <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="text-center mt-4">
+          <button @click="deleteAccount" class="bg-red-500 text-white font-medium px-3 py-1 rounded hover:bg-red-600">Delete Account</button>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      attacks: [
+        { name: 'Light Attack' },
+        { name: 'Heavy Attack' },
+        { name: 'Special Attack' },
+      ],
     };
-  </script>
+  },
+  methods: {
+    editAttack(index) {
+      // Handle editing logic, e.g., send data to a server or store changes
+    },
+    deleteAttack(index) {
+      this.attacks.splice(index, 1);
+    },
+    addNewAttack() {
+      this.attacks.push({ name: '' });
+    },
+    deleteAccount() {
+      // Handle account deletion logic here
+    },
+  },
+};
+</script>
